6932135d74f7102d07614c20f02bab4579a48a59
Liam Dwinnell <laimd@udel.edu>
2020-05-07 11:26:25 -0400

    Updates

---
+++ beta/precache-manifest.2fc252d92918e15b074256a2642eef48.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a156691f96a8cf6c8c2646bc084a8f",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/bars_tile.png"
+  },
+  {
+    "revision": "4a4c215ee8759b023e8c673dcbc70b00",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/black_key.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "1d3e7fd111b9e6810500a7ace1f461d9",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/menu_images/menu_background.png"
+  },
+  {
+    "revision": "9e3d5ac021b1d88387b70eced8bc2b15",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/menu_images/note_mode.png"
+  },
+  {
+    "revision": "8d636aec319861bcd966521ca10c0696",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/menu_images/select_backdrop.png"
+  },
+  {
+    "revision": "702b1ee000835c083351e2e1e9fe3445",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/menu_images/selector.png"
+  },
+  {
+    "revision": "745e0e981c3234770ae5058c5d6bd922",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/menu_images/song_mode.png"
+  },
+  {
+    "revision": "7efcc48bc9ef21219e3ea64096684cc2",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/menu_images/story_mode.png"
+  },
+  {
+    "revision": "686e73af5be2ab53971da75aa8cd1cbf",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/menu_images/title.png"
+  },
+  {
+    "revision": "e12899d0c7f223affd0281ef99bf2402",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/level_select_1.png"
+  },
+  {
+    "revision": "897095d4e4bd309a3572b5d9773b2b56",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/level_select_2.png"
+  },
+  {
+    "revision": "bebed44c240a616b5928230e0029a045",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/level_select_3.png"
+  },
+  {
+    "revision": "a223cd6bb7d9c450122c6f0843bbd81a",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/level_select_4.png"
+  },
+  {
+    "revision": "c3f48b748cd0f7391b61eaebc836d9a5",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/level_select_5.png"
+  },
+  {
+    "revision": "c5b9e2f691ea4c294430d757e336d98f",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/level_select_6.png"
+  },
+  {
+    "revision": "2b702384dbbcb1821ad73ef3be51929a",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/level_select_7.png"
+  },
+  {
+    "revision": "9c293f2c824ef3a63f562af3182ea22e",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/level_select_8.png"
+  },
+  {
+    "revision": "46356afe55fa3cea9cbe73ad442cad47",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/note_high.txt"
+  },
+  {
+    "revision": "468dd6136ed428e64a7d251ef93d0596",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/q_a_6.png"
+  },
+  {
+    "revision": "410cc2505326888d3b8ff038d9bded60",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/q_b_7.png"
+  },
+  {
+    "revision": "4ffb413c790ff0b4182d22cb826a7752",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/q_c_1.png"
+  },
+  {
+    "revision": "b5152c77509d84b2bcbfb91908f1ee87",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/q_c_8.png"
+  },
+  {
+    "revision": "5f6f4a61713e118112866d78e6f9d766",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/q_d_2.png"
+  },
+  {
+    "revision": "e252707b2a4146f129870f8aa8fcd5b7",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/q_e_3.png"
+  },
+  {
+    "revision": "e799d0acb01b5431d58cf38da4be791b",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/q_f_4.png"
+  },
+  {
+    "revision": "25cf560195768db16586972ae0a30122",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/q_g_5.png"
+  },
+  {
+    "revision": "a07479f515d4f88e7e6c2d20b7f755b1",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/select_level1.png"
+  },
+  {
+    "revision": "ef4b9e8cd9d8aa9071fda18154cd3309",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/select_level2.png"
+  },
+  {
+    "revision": "bba32f39a91ba47269ecaf95513a0e20",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/select_level3.png"
+  },
+  {
+    "revision": "a7b390a093015f0554fe7d17414b6fbd",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/select_level4.png"
+  },
+  {
+    "revision": "c4c1b76b70ee252f791739c37b639235",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/select_level5.png"
+  },
+  {
+    "revision": "71bbe2b698e54c7116ff4d3737c50778",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/select_level6.png"
+  },
+  {
+    "revision": "ec925a3f2708ba92ae1c12e3f5e888ea",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/select_level7.png"
+  },
+  {
+    "revision": "9c293f2c824ef3a63f562af3182ea22e",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/select_level8.png"
+  },
+  {
+    "revision": "49be57b2468ddeee1499a2fac1d6c25c",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/note_mode/temp_back.jpg"
+  },
+  {
+    "revision": "6e387363e75074ca8edc901f26b349c4",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/piano_scene_1.jpg"
+  },
+  {
+    "revision": "61123757a7c10a7d411e62fe95340973",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/piano_scene_2.jpg"
+  },
+  {
+    "revision": "52086ac1d0ad8eac7990e4eef6726f17",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/piano_scene_3.jpg"
+  },
+  {
+    "revision": "216f329163df8cea3cf3d61449571e61",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/a.m4a"
+  },
+  {
+    "revision": "9dc32d54026b51a427f23117a456bd32",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/a.mp3"
+  },
+  {
+    "revision": "e8faeb793a67445875f26acff266f8fe",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/b.m4a"
+  },
+  {
+    "revision": "2c959d9c5378e75965571f1bade4fdfc",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/b.mp3"
+  },
+  {
+    "revision": "9588bdb2fb480a9ae5c2d563bf66995a",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/c.m4a"
+  },
+  {
+    "revision": "1dbc1135bfc4ce0d139580a236c07ba8",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/c.mp3"
+  },
+  {
+    "revision": "63c23cc6e374ce47443fe223c5047a82",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/c2.m4a"
+  },
+  {
+    "revision": "218696e38a93aac8ea9b8663ebac5326",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/c2.mp3"
+  },
+  {
+    "revision": "01f6db0f79db432a721b5b63d785460f",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/d.m4a"
+  },
+  {
+    "revision": "27aea9530173cde4283f7ec8519d3e0f",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/d.mp3"
+  },
+  {
+    "revision": "e98941eed8ae50f68d945d270972a92f",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/e.m4a"
+  },
+  {
+    "revision": "6e8e13010f812c846757e292bbc764c9",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/e.mp3"
+  },
+  {
+    "revision": "85bc29885168c0e73170887d92010beb",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/f.m4a"
+  },
+  {
+    "revision": "69d65370798e738435786d4eef5cdf60",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/f.mp3"
+  },
+  {
+    "revision": "ff18a4b4b7bd65a67657341c285b0688",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/g.m4a"
+  },
+  {
+    "revision": "f80a6a95432b7b9289b684bb146fcc53",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/sounds/g.mp3"
+  },
+  {
+    "revision": "2eccd3b7f8a93d8e74194c592c1e441d",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaassets/white_key.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-11-william-dwinnell/betafavicon.ico"
+  },
+  {
+    "revision": "255c766b5aba5fe70450",
+    "url": "/educational-game-project-team-11-william-dwinnell/betagen.main.8907bbffe5b3b858cd81.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/educational-game-project-team-11-william-dwinnell/betagen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "091f6611e3d41244fa3b80a227105804",
+    "url": "/educational-game-project-team-11-william-dwinnell/betaindex.html"
+  },
+  {
+    "revision": "cc5f13cba7a5be4da9e43221c4a17fc2",
+    "url": "/educational-game-project-team-11-william-dwinnell/betamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-game-project-team-11-william-dwinnell/betasw.js"
+  }
+]);
---
+++ beta/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-game-project-team-11-william-dwinnell/betaprecache-manifest.2fc252d92918e15b074256a2642eef48.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
